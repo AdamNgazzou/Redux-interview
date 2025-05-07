ca9b625aa4e7be988ddfd2e3e652851c4a08d84f
Adam Ngazzou <117188411+AdamNgazzou@users.noreply.github.com>
2025-05-07 15:43:28 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,43 @@ import React from "react"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react";
 import store,{persistor} from "~/store"
-
+import { DefaultSeo, OrganizationJsonLd } from "next-seo"
+import SEOConfig from "~/config/seo-config"
 function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <Component {...pageProps} />
+        <DefaultSeo {...SEOConfig} />
+          <OrganizationJsonLd
+            type="Corporation"
+            logo={`${process.env.NEXT_PUBLIC_SITE_URL}/logo.png`}
+            legalName="Your Company Legal Name"
+            name="Your Company"
+            address={{
+              streetAddress: "123 Main St",
+              addressLocality: "City",
+              addressRegion: "State",
+              postalCode: "12345",
+              addressCountry: "US",
+            }}
+            contactPoint={[
+              {
+                telephone: "+1-800-123-4567",
+                contactType: "customer service",
+                email: "support@yourcompany.com",
+                areaServed: "US",
+                availableLanguage: ["English"],
+              },
+            ]}
+            sameAs={[
+              "https://www.facebook.com/your-company",
+              "https://www.instagram.com/your-company",
+              "https://www.linkedin.com/company/your-company",
+              "https://twitter.com/your-company",
+            ]}
+            url={process.env.NEXT_PUBLIC_SITE_URL || ""}
+          />
+        <Component {...pageProps} />
       </PersistGate>
     </Provider>
   )
